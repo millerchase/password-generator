022b4cf5f45201908b5c84c24efe7c518f70ceee
CMiller <mlr.chase40@gmail.com>
2022-04-07 18:37:18 -0500

    add fully random password creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,31 +3,27 @@
 // create criteria options
 const lowerCaseObj = {
   string: "lower case",
-  chars: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-  selected: false
+  chars: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 };
 
 const upperCaseObj = {
   string: "upper case",
-  chars: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-  selected: false
+  chars: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 };
 
 const numericObj = {
   string: "numeric",
-  chars: [0,1,2,3,4,5,6,7,8,9],
-  selected: false
+  chars: [0,1,2,3,4,5,6,7,8,9]
 };
 
 const specialObj = {
   string: "special",
-  chars: [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"],
-  selected: false
+  chars: [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"]
 };
 
 // choose character at random
 const chooseRandom = max => {
-  Math.floor(Math.random()*max);
+  return Math.floor(Math.random()*max);
 }
 
 // Create password criteria
@@ -52,9 +48,47 @@ const passwordCriteriaHandler = () => {
     passwordCriteriaHandler();
   }
   // pass on selected criteria
-  generatePassword(selectedCriteria);
+  return generatePassword(selectedCriteria);
 }
 
+// get password length
+const getPasswordLength = () => {
+  passwordLength = parseFloat(prompt("How many character would you like your password? Must be between 8 and 128 characters."));
+  if(passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 charcters!");
+    getPasswordLength();
+  } else if (!passwordLength) {
+    alert("Numbers Only!");
+    getPasswordLength();
+  } else {
+    return passwordLength;
+  }
+}
+
+// generate password
+const generatePassword = selectedCriteria => {
+  // choose password length
+  let passwordLength = getPasswordLength();
+  let createdPassword = [];
+
+  if (selectedCriteria.length > 1) {
+    for (let i = 0; i < passwordLength; i++){
+      // choose random charcter set then random character
+      let charSetSelected = selectedCriteria[chooseRandom(selectedCriteria.length)];
+      let charSelected = charSetSelected[chooseRandom(charSetSelected.length)];
+      
+      createdPassword.push(charSelected);
+    }
+  } else {
+    for(let i = 0; i < passwordLength; i++){
+      // choose random character
+      let charSelected = selectedCriteria[0][chooseRandom(selectedCriteria[0].length)];
+
+      createdPassword.push(charSelected);
+    }
+  };
+  return createdPassword.join('');
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -62,7 +96,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  var password = generatePassword();
+  var password = passwordCriteriaHandler();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -70,4 +104,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", passwordCriteriaHandler);
+generateBtn.addEventListener("click", writePassword);
+
